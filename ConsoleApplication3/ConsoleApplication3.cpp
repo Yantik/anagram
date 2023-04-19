3a5ae051c37b191ada62e611df5fcfe4f8d93251
foxme <foxme@DESKTOP-HM8KCSB>
2023-04-19 17:05:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> 
+#include <algorithm> 
+#include <string> 
+
+using namespace std;
+
+int main() 
+{
+    string s1, s2;
+    cout << "Enter s1: ";
+    getline(cin, s1);
+    cout << "Enter s2 : ";
+    getline(cin, s2);
+    s1.erase(remove(s1.begin(), s1.end(), ' '), s1.end());
+    s2.erase(remove(s2.begin(), s2.end(), ' '), s2.end());
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    s1 == s2 ? cout << "like.": cout << "not like.";
+}
